53daff4dde2f4c195d70802f044d918934893750
Webn-Benjamin <benjamin.cassa@gmail.com>
2022-12-08 16:22:09 +0100

    Modify qte + reload price and quantity work

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,73 +1,110 @@
-function getStorage() {
-  const Localstorage = JSON.parse(localStorage.getItem("product"));
-  return Localstorage;
+// récupération du localStorage
+let localstorage = JSON.parse(localStorage.getItem("product"));
+// initialisation des fonctions
+// Récupération des prix des produits de l'Api
+function getProductapi() {
+  for (let p = 0; p < localstorage.length; p++) {
+    fetch("http://localhost:3000/api/products/" + localstorage[p].id)
+      .then((response) => response.json())
+      .then((data) => {
+        displayProduct(data, localstorage[p]);
+        productTemplate(data, localstorage[p]);
+        totalPrix(data, localstorage[p]);
+        modifyQtt(data, localstorage[p]);
+      });
+  }
 }
-getStorage();
+getProductapi();
+
 // Selection de la section des produits
 let emptyCart = document.querySelector("#cart__items");
 
-function displayProduct() {
-  if (getStorage() === null) {
+function displayProduct(priceApi, productcart) {
+  if (localstorage === null) {
     let createpEmpty = document.createElement("p");
     createpEmpty.textContent = "Votre panier est vide";
     emptyCart.appendChild(createpEmpty);
   } else {
-    displayTemplate = productTemplate();
-    emptyCart.insertAdjacentHTML("beforeend", displayTemplate);
+    displayTemplate = productTemplate(priceApi, productcart);
+    displayPrice = emptyCart.insertAdjacentHTML("beforeend", displayTemplate);
   }
 }
-displayProduct();
 
-function productTemplate(price) {
-  let productPrice = price
-  console.log(productPrice)
-  let kanap = getStorage();
+function productTemplate(price, productcart) {
   let display = "";
-  for (let i = 0; i < kanap.length; i++) {
-    display += `
-          <article class="cart__item" data-id="${kanap[i].id}" data-color="${kanap[i].colors}">
-                <div class="cart__item__img">
-                  <img src="${kanap[i].imageUrl}" alt="Photographie d'un canapé">
-                </div>
-                <div class="cart__item__content">
-                  <div class="cart__item__content__description">
-                    <h2>${kanap[i].name}</h2>
-                    <p>${kanap[i].colors}</p>
-                    <p>${productPrice}</p>
-                  </div>
-                  <div class="cart__item__content__settings">
-                    <div class="cart__item__content__settings__quantity">
-                      <p>Qté : </p>
-                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${kanap[i].quantity}">
-                    </div>
-                    <div class="cart__item__content__settings__delete">
-                      <p class="deleteItem">Supprimer</p>
-                    </div>
-                  </div>
-                </div>
-              </article>
-        `;
-  }
+  display += `
+    <article class="cart__item" data-id="${productcart.id}" data-color="${productcart.colors}">
+    <div class="cart__item__img">
+    <img src="${productcart.imageUrl}" alt="Photographie d'un canapé">
+    </div>
+    <div class="cart__item__content">
+    <div class="cart__item__content__description">
+    <h2>${productcart.name}</h2>
+    <p>${productcart.colors}</p>
+    <p>${price.price} €</p>
+    </div>
+    <div class="cart__item__content__settings">
+    <div class="cart__item__content__settings__quantity">
+    <p>Qté : </p>
+    <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${productcart.quantity}">
+    </div>
+    <div class="cart__item__content__settings__delete">
+    <p class="deleteItem">Supprimer</p>
+    </div>
+    </div>
+    </div>
+    </article>
+    `;
   return display;
 }
 
-function getApiproduct() {
-  const localstorage = getStorage();
-  for (let a = 0; a < localstorage.length; a++) {
-    fetch("http://localhost:3000/api/products/" + localstorage[a].id)
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const productprice = data.price;
-        displayPrice = productTemplate(productprice);
-        // const createpPrice = document.createElement("p");
-        // createpPrice.textContent = productprice + " €";
-        // console.log(createpPrice);
-        // const selectdivprice = document.querySelector(
-        //   ".cart__item__content__description"
-        // );
-      });
+function totalQteProduits() {
+  let InitialValue = 0;
+  let productLocalstorage = localstorage;
+  for (let q in productLocalstorage) {
+    // La fonction parseInt() permet de convertir une chaîne en nombre entier.
+    const qte = parseInt(productLocalstorage[q].quantity);
+
+    // Donner la valeur parseInt à la valeur initiale.
+    InitialValue += qte;
+  }
+  // Selectionner la quantité puis l'injecter
+  const totalQuantity = document.getElementById("totalQuantity");
+  totalQuantity.textContent = InitialValue;
+}
+totalQteProduits();
+
+function totalPrix(product, productLocalstorage) {
+  let initialPrice = 0;
+  for (let q in localstorage) {
+    let price = parseInt(product.price * productLocalstorage.quantity);
+    initialPrice += price;
+  };
+  const totalPrice = document.getElementById("totalPrice");
+  totalPrice.textContent = initialPrice;
+}
+
+function modifyQtt(price, productcart) {
+  let qttModif = document.querySelectorAll(".itemQuantity");
+
+  for (let k = 0; k < qttModif.length; k++) {
+    qttModif[k].addEventListener("change", (event) => {
+      event.preventDefault();
+
+      let quantityModif = localstorage[k].quantity;
+      let qttModifValue = qttModif[k].valueAsNumber;
+
+      const resultFind = localstorage.find(
+        (el) => el.qttModifValue !== quantityModif
+      );
+      resultFind.quantity = qttModifValue;
+      localstorage[k].quantity = resultFind.quantity;
+      localStorage.setItem("product", JSON.stringify(localstorage));
+
+      // refresh rapide
+      totalPrix(price, productcart);
+      totalQteProduits();
+      // location.reload();
+    });
   }
 }
-getApiproduct();
